14f794fc8bee07efb585ee8ae630dadb58097273
Mohamed8706 <133748287+Mohamed8706@users.noreply.github.com>
2025-06-02 15:28:01 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,32 +1,32 @@
 let scrollToTop = document.querySelector(".scroll");
 
-document.body.onscroll = function () {
-    if (scrollY > 200) {
-        scrollToTop.style.bottom = "20px"
-    }
-    else {
-        scrollToTop.style.bottom = "-100px"
-        
-    }
-}
 
-scrollToTop.onclick = function() {
-    document.documentElement.scrollTop = 0
+if (scrollToTop) {
+    document.body.onscroll = function () {
+        if (scrollY > 200) {
+            scrollToTop.style.bottom = "20px";
+        } else {
+            scrollToTop.style.bottom = "-100px";
+        }
+    };
+    
+    scrollToTop.onclick = function() {
+        document.documentElement.scrollTop = 0;
+    };
 }
 
 const sections = document.querySelectorAll("section:not(#projects) *");
 
-const revealSection = function(enteries, observer) {
-    const [entry] = enteries;
+const revealSection = function(entries, observer) {
+    const [entry] = entries;
     if (!entry.isIntersecting) return;
-
     entry.target.classList.add("apparate");
-    observer.unobserve(entry.target)
-}
+    observer.unobserve(entry.target);
+};
 
-const observer = new IntersectionObserver(revealSection());
+
+const observer = new IntersectionObserver(revealSection);
 
 sections.forEach((el) => {
-    observer.observe(el) 
-
-})
+    observer.observe(el);
+});
